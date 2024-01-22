c5f7559e29c12f4c6d4bb19b17c947eea2fd97cc
Vincent <hailinfufu@outlook.com>
2024-01-22 20:49:39 +0800

    update

--- docker/build_base_container.sh
+++ docker/build_base_container.sh
@@ -2,10 +2,10 @@
 
 set -x -e
 
-PY_VERSION=${1:-"3.8"}
-TF_VERSION=${2:-"2.9.1"}
-CUDA_VERSION=${3:-"11.6.2"}
-OS_VERSION=${4:-"20.04"}
+PY_VERSION=${1:-"3.10"}
+TF_VERSION=${2:-"2.13.0"}
+CUDA_VERSION=${3:-"11.8.0"}
+OS_VERSION=${4:-"22.04"}
 
 docker build \
     -f base_container.Dockerfile \
@@ -15,6 +15,6 @@ docker build \
     --build-arg PY_VERSION=${PY_VERSION} \
     --build-arg OS_VERSION=${OS_VERSION} \
     --target base_container \
-    -t hailinfufu/deepray-base:24.01-py${PY_VERSION}-tf${TF_VERSION}-cu${CUDA_VERSION}-ubuntu${OS_VERSION} ./
+    -t hailinfufu/deepray-base:latest-py${PY_VERSION}-tf${TF_VERSION}-cu${CUDA_VERSION}-ubuntu${OS_VERSION} ./
 
-docker push hailinfufu/deepray-base:24.01-py${PY_VERSION}-tf${TF_VERSION}-cu${CUDA_VERSION}-ubuntu${OS_VERSION}
+docker push hailinfufu/deepray-base:latest-py${PY_VERSION}-tf${TF_VERSION}-cu${CUDA_VERSION}-ubuntu${OS_VERSION}
